edef1977b259a0c688e0c636c2c7175a5c71904c
BreeUbuntu <breelan.collins@gmail.com>
2017-08-03 10:06:02 -0500

    added gitignore and updates to GooberSpeechlet

--- src/com/goobers/speechlet/GoobersSpeechlet.java
+++ src/com/goobers/speechlet/GoobersSpeechlet.java
@@ -1,6 +1,7 @@
 package com.goobers.speechlet;
 
 import com.amazon.speech.slu.Intent;
+import com.amazon.speech.slu.Slot;
 import com.amazon.speech.speechlet.IntentRequest;
 import com.amazon.speech.speechlet.LaunchRequest;
 import com.amazon.speech.speechlet.Session;
@@ -9,11 +10,12 @@ import com.amazon.speech.speechlet.SessionStartedRequest;
 import com.amazon.speech.speechlet.Speechlet;
 import com.amazon.speech.speechlet.SpeechletException;
 import com.amazon.speech.speechlet.SpeechletResponse;
+import com.amazon.speech.speechlet.User;
 import com.amazon.speech.ui.OutputSpeech;
 import com.amazon.speech.ui.PlainTextOutputSpeech;
 import com.amazon.speech.ui.Reprompt;
+import com.goobers.db.AccountDAO;
 import com.goobers.resource.Account;
-import com.goobers.model.User;
 
 /**
  * This is just an example to help us figure out what we need in the model
@@ -25,8 +27,11 @@ public class GoobersSpeechlet implements Speechlet {
 	private static final String SESSION_STAGE = "stage";
 	private static final Object LOGIN = 1;
 	
+//	TODO make these session vars
 	private Account currentAccount;
 	private User currentUser;
+	private String name;
+	private AccountDAO accountDao;
 
 	@Override
 	public void onSessionStarted(SessionStartedRequest request, Session session) throws SpeechletException {
@@ -43,13 +48,14 @@ public class GoobersSpeechlet implements Speechlet {
 	private SpeechletResponse accountLoginStart(Session session) {
 		String speechOutput = "";
 
-        // Reprompt com.amazon.speech will be triggered if the user doesn't respond.
-        String repromptText = "What is your account's nickname?";
+        // Reprompt speech will be triggered if the user doesn't respond.
+        String repromptText = "What is your name?";
 
         // The stage variable tracks the phase of the dialogue.
         // When this function completes, it will be on stage 1.
         session.setAttribute(SESSION_STAGE, LOGIN);
-        speechOutput = "What is your account's nickname?";
+//        speechOutput = "What is your name?";
+        speechOutput = repromptText;
        
         OutputSpeech outputSpeech, repromptOutputSpeech;
         outputSpeech = new PlainTextOutputSpeech();
@@ -59,10 +65,7 @@ public class GoobersSpeechlet implements Speechlet {
         Reprompt reprompt = new Reprompt();
         reprompt.setOutputSpeech(repromptOutputSpeech);
         return SpeechletResponse.newAskResponse(outputSpeech, reprompt);
-//
-//        SpeechletResponse response = newAskResponse(speechOutput, false,
-//                repromptText, false);
-//        return response;
+
 	}
 
 
@@ -71,7 +74,17 @@ public class GoobersSpeechlet implements Speechlet {
         Intent intent = request.getIntent();
         String intentName = (intent != null) ? intent.getName() : null;
 
-        if ("viewAccountIntent".equals(intentName)) {
+        if ("firstNameIntent".equals(intentName)) {
+//        	TODO set the name as private instance var or session variable
+        	Slot nameSlot = intent.getSlot("FirstName");
+        	name = nameSlot.getValue();
+        	return handleNameIntent(session);
+        } else if("passphraseIntent".equals(intentName)) {
+//        	TODO store passphrase as a session variable
+        	Slot passSlot = intent.getSlot("");
+        	return handlePassphraseItent(session);
+        }
+        else if ("viewAccountIntent".equals(intentName)) {
             return handleViewAccountIntent(session);
         } else if ("viewInsuranceIntent".equals(intentName)) {
             return handleViewInsuranceIntent(session);
@@ -133,6 +146,37 @@ public class GoobersSpeechlet implements Speechlet {
 	}
 
 
+	private SpeechletResponse handlePassphraseItent(Session session) {
+//		TODO check that passphrase/pin matches
+//			 --> query the db
+		
+//		TODO - initialize currentUser
+//		if passphrase with account owner, set permissions to View
+//		currentUser = new User()...
+//		else if pin with account owner, set permissions to Manage
+//		else if passphrase with account user, set permissions to View
+		
+		
+//		else reprompt for correct pin/passphrase
+		
+		return null;
+	}
+
+//	TODO decide if we want to handle bad user names, or wait until we 
+//	have a bad username/password
+	private SpeechletResponse handleNameIntent(Session session) {
+		
+//		TODO check if name belongs to a user
+//			 --> query the db to see if the account contains that user
+		
+//		TODO if name is the account owner, ask for pin or passphrase
+//			 else if account user ask for passphrase
+//			 else reprompt for correct user name
+		
+		return null;
+	}
+
+
 	private SpeechletResponse handleUpdateInsuranceYesIntent(Session session) {
 		// TODO Auto-generated method stub
 		return null;
@@ -140,7 +184,12 @@ public class GoobersSpeechlet implements Speechlet {
 
 
 	private SpeechletResponse handleMenuIntent(Session session) {
-		// TODO Auto-generated method stub
+		if(currentUser != null) {
+			
+		} else {
+//			TODO put them back in the login state
+		}
+		
 		return null;
 	}
 
